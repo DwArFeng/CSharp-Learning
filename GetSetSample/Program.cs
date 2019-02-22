d9394c09a766830d499c50bc77c264703e396529
DwArFeng <915724865@qq.com>
2019-02-22 11:31:08 +0800

    : 将所有代码的命名空间更改到了CoreCSharpSamples下
    Signed-off-by: DwArFeng <915724865@qq.com>

--- GetSetSample/Program.cs
+++ GetSetSample/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace GetSetSample {
+namespace CoreCSharpSamples.GetSetSample {
 
     class Program {
 
--- VariablesSample/Program.cs
+++ VariablesSample/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace VariablesSample {
+namespace CoreCSharpSamples.VariablesSample {
 
     class Program {
 
